4bb99cc6e724db8f4f25461a407702c65277edf0
Jyotirmay Swarnakar <jyotirmayswarnakar79@gmail.com>
2025-03-20 15:49:38 +0530

    Update python_assignment.py

--- python_assignment.py
+++ python_assignment.py
@@ -11,3 +11,16 @@ print("The largest number is:", largest)
 
 #Q2:
 
+filename = "jyotirmay.txt"  # Your file name
+
+with open(filename, 'r') as file:
+    text = file.read()
+    
+lines = text.split('\n')
+line_count = len(lines)
+word_count = len(text.split())
+char_count = len(text)
+
+print("Lines:", line_count)
+print("Words:", word_count)
+print("Characters:", char_count)
