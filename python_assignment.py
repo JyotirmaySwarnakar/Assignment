0d58311c87dc37fb79fa035657b075a6dd05577a
Jyotirmay Swarnakar <jyotirmayswarnakar79@gmail.com>
2025-03-20 15:44:03 +0530

    Update and rename classassignment.py to python_assignment.py

---
+++ python_assignment.py
@@ -0,0 +1,13 @@
+#Q1:
+numbers = [5, 12, 3, 8, 9, 1, 23, 6]
+
+largest = numbers[0]  
+
+for number in numbers:
+    if number > largest:
+        largest = number  
+
+print("The largest number is:", largest)
+
+#Q2:
+
